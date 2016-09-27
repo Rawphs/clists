dd30604581c4b8f3d6d8ea06346f112d3d77e4cc
RWOverdijk <r.w.overdijk@gmail.com>
2016-09-27 11:59:47 +0200

    refactor(project): Moved logic

--- index.js
+++ index.js
@@ -1,344 +1,71 @@
 "use strict";
 
-const mysql    = require('mysql');
-const inquirer = require('inquirer');
-const path     = require('path');
-const fs       = require('fs');
+const prompt = require('./lib/prompt');
+const setup  = require('./lib/config');
 
-let home       = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-let configPath = path.join(home, '.clists');
-let data       = {todos: []};
-let connection;
+let data;
 
-setup();
+function clists() {
+  return setup()
+    .then(storage => {
+      data = storage;
 
-// Set up connection w/ database.
-
-function setup() {
-  return new Promise((resolve) => {
-    return fs.readFile(`${configPath}.json`, 'utf8', (error, result) => {
-      if (error) {
-        return saveConfig();
-      }
-
-      let config = JSON.parse(result);
-
-      connection = mysql.createConnection(config.database);
-      return resolve(connection.connect());
-    });
-  })
-    .then(() => {
-      return mainMenu();
+      return main();
     });
 }
 
-// Save clists.json.
-
-function saveConfig() {
-  return inquirer.prompt([{
-    name   : 'host',
-    message: 'HOST',
-    type   : 'input'
-  }, {
-    name   : 'user',
-    message: 'USERNAME',
-    type   : 'input'
-  }, {
-    name   : 'database',
-    message: 'DATABASE',
-    type   : 'input'
-  }])
-    .then(answers => {
-      let config = {
-        database: {
-          host    : answers.host,
-          user    : answers.user,
-          database: answers.database
-        }
-      };
-
-      connection = mysql.createConnection(config.database);
-      connection.connect();
-
-      return fs.writeFile(`${configPath}.json`, JSON.stringify(config), 'utf8', error => {
-        if (error) {
-          throw error;
-        }
-      });
-    })
-    .then(() => {
-      connection.query('CREATE TABLE Lists (list_id INT AUTO_INCREMENT PRIMARY KEY, ' +
-        'list_name VARCHAR(20) NOT NULL)', error => {
-        if (error) {
-          throw error;
-        }
-      });
-
-      connection.query('CREATE TABLE Todos (todo_id INT AUTO_INCREMENT PRIMARY KEY, ' +
-        'todo TINYTEXT NOT NULL, done BOOLEAN NOT NULL, list INT, ' +
-        'FOREIGN KEY (list) REFERENCES Lists(list_id) ON DELETE CASCADE)', error => {
-        if (error) {
-          throw error;
-        }
-      });
-
-      return setup();
-    });
-}
-
-// Create or select a list.
-
-function mainMenu() {
-  return inquirer.prompt([{
-    name   : 'action',
-    message: '***Welcome to the mighty ToDo List Maker, #1 in Omicron Persei 8!***',
-    type   : 'list',
-    choices: ['Create a new list', 'View existent list']
-  }])
+function main() {
+  return prompt.mainMenu()
     .then(answer => {
-
       if (answer.action === 'Create a new list') {
-        return createNewList();
+        return prompt.listCreation()
+          .then(answer => data.createList(answer.listName))
+          .then(() => options());
       } else {
-        return listSelection();
+        return data.selectList()
+          .then(lists => prompt.viewLists(lists))
+          .then(answer => data.list.id = answer.list)
+          .then(() => options());
       }
-    })
-    .then(() => {
-      return editList();
     });
 }
 
-// Edit options (add, remove, delete).
+function options() {
+  return data.fetchTodos()
+    .then(data => {
+      data.listPreview();
 
-function editList() {
-
-  return fetchTodos(data.list_id)
-    .then(() => {
-      listPreview();
-
-      return inquirer.prompt([{
-        name   : 'editList',
-        message: 'Choose one option bellow:',
-        type   : 'list',
-        choices: ['Add To Do', 'Mark as done', 'Remove To Do', 'Delete list', 'Quit']
-      }]);
+      return prompt.editList();
     })
-    .then(answers => {
-      if (answers.editList === 'Mark as done') {
-        return changeStatus(data.todos); // @todo: todosArray does not exist anymore
+    .then(answer => {
+      if (answer.option === 'Add todo') {
+        return prompt.add()
+          .then(answer => data.addTodo(answer.add))
+          .then(() => options());
 
-      } else if (answers.editList === 'Add To Do') {
-        return addTodo();
+      } else if (answer.option === 'Remove todo') {
+        return prompt.remove(data.list.todos)
+          .then(answer => data.removeTodos(answer.remove))
+          .then(() => options());
 
-      } else if (answers.editList === 'Remove To Do') {
-        console.log(data.todos);
-        return removeTodo(data.todos); // @todo: todosArray does not exist anymore
+      } else if (answer.option === 'Delete list') {
+        return data.selectList()
+          .then(lists => prompt.deleteList(lists))
+          .then(answer => data.deleteList(answer.confirm))
+          .then(() => options());
 
-      } else if (answers.editList === 'Delete list') {
-        return deleteList();
+      } else if (answer.option === 'Mark as done') {
+        return prompt.setTodoStatus(data.list.todos)
+          .then(todos => data.setTodoStatus(todos.done))
+          .then(() => options());
+
+      } else if (answer.option === 'Change list') {
+        return main();
 
       } else {
-        return connection.end();
+        data.connection.end();
       }
     });
 }
 
-// Create new list.
-
-function createNewList() {
-  return inquirer.prompt([{
-    name   : 'newListName',
-    message: 'Write your new list name:',
-    type   : 'input'
-  }])
-    .then(answer => {
-      connection.query(`INSERT INTO Lists (list_name) VALUES (?)`, [answer.newListName], (error, result) => {
-        if (error) {
-          throw error;
-        }
-
-        data.list_id = result.insertId;
-      });
-    });
-}
-
-// Show lists available for selection.
-
-function listSelection() {
-
-  return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM Lists', (error, results) => {
-      if (error) {
-        return reject(error);
-      }
-
-      let list = [];
-
-      results.forEach(result => {
-        list.push({name: result.list_name, value: result.list_id});
-      });
-
-      return resolve(list);
-    });
-  })
-    .then(list => {
-
-      return inquirer.prompt([{
-        name   : 'list',
-        message: 'Select your list:',
-        type   : 'list',
-        choices: list
-      }])
-    })
-    .then(answer => {
-      data.list_id = answer.list;
-    });
-}
-
-// Set To Do current status (boolean).
-
-function changeStatus(todos) {
-  return inquirer.prompt([{
-    name   : 'done',
-    message: 'Mark task as completed. (Selecting same task again will set its status as ',
-    type   : 'checkbox',
-    choices: todos
-  }])
-    .then(answers => {
-      data.todos.forEach(todo => {
-        answers.done.forEach(id => {
-          if (todo.value === id && todo.done) {
-            return connection.query('UPDATE Todos SET done=? WHERE todo_id=?', [0, id], error => {
-              if (error) {
-                throw error;
-              }
-            });
-          } else if (todo.value === id) {
-            return connection.query('UPDATE Todos SET done=? WHERE todo_id=?', [1, id], error => {
-              if (error) {
-                throw error;
-              }
-            });
-          }
-        });
-      });
-    })
-    .then(() => {
-      editList();
-    });
-}
-
-// Insert To Do into the table.
-
-function addTodo() {
-  return inquirer.prompt([{
-    name   : 'add',
-    message: 'Write your new todo:',
-    type   : 'input'
-  }])
-    .then(answer => {
-      return connection.query('INSERT INTO Todos (todo, done, list) VALUES (?, ?, ?)',
-        [answer.add, 0, data.list_id], error => {
-          if (error) {
-            throw error;
-          }
-        });
-    })
-    .then(() => {
-      editList();
-    });
-}
-
-// Remove To Do from the table.
-
-function removeTodo(todos) {
-  return inquirer.prompt([{
-    name   : 'remove',
-    message: 'Choose todo to remove:',
-    type   : 'checkbox',
-    choices: todos
-  }])
-    .then(answers => {
-      answers.remove.forEach(todo => {
-        console.log(todo);
-        return connection.query('DELETE FROM Todos WHERE todo_id=?', [todo], error => {
-          if (error) {
-            throw error;
-          }
-        });
-      });
-
-      return  editList();
-    });
-}
-
-// Delete List and associated To Dos from the tables.
-
-function deleteList() {
-  return inquirer.prompt([{
-    name   : 'confirmation',
-    message: 'Are you sure you want to delete this list? This action cannot be undone.',
-    type   : 'confirm'
-  }])
-    .then(answer => {
-      if (answer.confirmation) {
-        connection.query('DELETE FROM Lists WHERE list_id=?', [data.list_id], error => {
-          if (error) {
-            throw error;
-          }
-        });
-        connection.query('DELETE FROM Todos WHERE list=?', [data.list_id], error => {
-          if (error) {
-            throw error;
-          }
-        });
-      }
-    })
-    .then(() => {
-      console.log('List deleted successfully.');
-
-      mainMenu();
-    });
-}
-
-// Fetch To Dos from selected List.
-
-function fetchTodos(listId) {
-  return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM Todos WHERE list=?', [listId], (error, results) => {
-      if (error) {
-        reject(error);
-      }
-
-      data.todos = [];
-
-      results.forEach(result => {
-        data.todos.push({
-          name : result.todo,
-          value: result.todo_id,
-          done : result.done
-        });
-      });
-
-      return resolve(data.todos);
-    });
-  });
-}
-
-// Log To Dos and its current status.
-
-function listPreview() {
-  console.log(`Here is a preview of your list:\n`);
-
-  data.todos.forEach(item => {
-
-    let checkbox = '[]';
-
-    if (item.done) {
-      checkbox = '[x]';
-    }
-
-    console.log(`${checkbox} ${item.name}\n`);
-  });
-}
-
+clists();
