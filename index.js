350d24be9105dc7a5963577e3b439c817a8119be
Rawphs <raphaela@spoonx.nl>
2016-09-29 10:31:27 +0200

    refactor(project): Moved logic to services

--- index.js
+++ index.js
@@ -1,96 +1,64 @@
 "use strict";
 
-const prompt = require('./lib/prompt');
-const setup  = require('./lib/config');
+const prompt      = require('./lib/prompt');
+const config      = require('./lib/config');
+const ListService = require('./lib/listService').ListService;
+const TodoService = require('./lib/todoService').TodoService;
 
-let data;
+let storage;
+let data = {};
 
 function clists() {
-  return setup()
-    .then(storage => {
-      data = storage;
+  return config.setup()
+    .then(config => {
+      storage = config;
 
-      return main();
+      return mainMenu();
     });
 }
 
-function main() {
+function mainMenu() {
   return prompt.mainMenu()
     .then(answer => {
-      if (answer.action === 'Create a new list') {
-        return prompt.listCreation()
-          .then(answer => data.createList(answer.listName))
-          .then(() => editOptions());
-      } else {
-        return getLists()
-          .then(lists => prompt.viewLists(lists))
-          .then(answer => data.list.id = answer.list)
-          .then(() => editOptions());
+      if (!answer.action) {
+        return ListService.createList(storage);
       }
+
+      return ListService.getLists(storage)
+        .then(lists  => prompt.viewLists(lists))
+        .then(answer => answer.list);
+    })
+    .then(id => {
+      data.id = id;
+
+      return editOptions();
     });
 }
 
 function editOptions() {
-  return data.fetchTodos()
-    .then(results => {
-      data.list.todos = [];
-
-      results.forEach(result => {
-        data.list.todos.push({
-          name : result.todo,
-          value: result.id,
-          done : result.done
-        });
-      });
-
-      data.listPreview();
-
+  return TodoService.fetchTodos(storage, data)
+    .then(() => {
       return prompt.editList();
     })
     .then(answer => {
-      if (answer.option === 'Add todo') {
-        return prompt.addTodo()
-          .then(answer => data.addTodo(answer.add))
-          .then(() => editOptions());
+      let option = answer.option;
 
-      } else if (answer.option === 'Remove todo') {
-        return prompt.removeTodo(data.list.todos)
-          .then(answer => data.removeTodos(answer.remove))
-          .then(() => editOptions());
-
-      } else if (answer.option === 'Delete list') {
-        return getLists()
-          .then(lists => prompt.deleteList(lists))
-          .then(answer => data.deleteList(answer.confirm))
-          .then(() => editOptions());
-
-      } else if (answer.option === 'Mark as done') {
-        return prompt.setTodoStatus(data.list.todos)
-          .then(todos => data.setTodoStatus(todos.done))
-          .then(() => editOptions());
-
-      } else if (answer.option === 'Change list') {
-        return main();
-
-      } else {
-        return data.connection.end();
+      if (!option.type) {
+        return storage.endConnection();
       }
+
+      if (option.type.includes('menu')) {
+        return mainMenu();
+      }
+
+      if (option.type.includes('list')) {
+        return ListService[option.action](storage)
+          .then(() => editOptions());
+      }
+      
+      return TodoService[option.action](storage, data)
+        .then(() => editOptions());
     });
-}
-
-function getLists() {
-  return new Promise(resolve => {
-    return data.fetchLists()
-      .then(result => {
-        let lists = [];
-
-        result.forEach(result => {
-          lists.push({name: result.list_name, value: result.list_id});
-        });
-
-        return resolve(lists);
-      });
-  });
 }
 
 clists();
