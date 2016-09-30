44057ec8b70706b6276843338307c9bd026860dd
Rawphs <raphaela@spoonx.nl>
2016-09-30 11:17:58 +0200

    refactor(project): Refactor, again and again.

--- index.js
+++ index.js
@@ -1,64 +1,7 @@
 "use strict";
 
-const prompt      = require('./lib/prompt');
-const config      = require('./lib/config');
-const ListService = require('./lib/listService').ListService;
-const TodoService = require('./lib/todoService').TodoService;
+const Clists = require('./lib/clists').Clists;
 
-let storage;
-let data = {};
+let app = new Clists();
 
-function clists() {
-  return config.setup()
-    .then(config => {
-      storage = config;
-
-      return mainMenu();
-    });
-}
-
-function mainMenu() {
-  return prompt.mainMenu()
-    .then(answer => {
-      if (!answer.action) {
-        return ListService.createList(storage);
-      }
-
-      return ListService.getLists(storage)
-        .then(lists  => prompt.viewLists(lists))
-        .then(answer => answer.list);
-    })
-    .then(id => {
-      data.id = id;
-
-      return editOptions();
-    });
-}
-
-function editOptions() {
-  return TodoService.fetchTodos(storage, data)
-    .then(() => {
-      return prompt.editList();
-    })
-    .then(answer => {
-      let option = answer.option;
-
-      if (!option.type) {
-        return storage.endConnection();
-      }
-
-      if (option.type.includes('menu')) {
-        return mainMenu();
-      }
-
-      if (option.type.includes('list')) {
-        return ListService[option.action](storage)
-          .then(() => editOptions());
-      }
-      
-      return TodoService[option.action](storage, data)
-        .then(() => editOptions());
-    });
-}
-
-clists();
+app.run();
